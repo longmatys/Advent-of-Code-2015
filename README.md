dc930dde606199224bacdad5e99c3938b05eb5b9
Martin <longmatys@gmail.com>
2023-12-04 00:22:18 +0100

    day 19 part 2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Advent-of-Code-2015
 There are a few interesting days:
+- day 19 - Part 2, Pekne stupidni, protoze to stacilo jen redukovat, nemohlo se to opakovat (coz v zadani nebylo). Inspirace byla zde: https://www.reddit.com/r/adventofcode/comments/3xflz8/comment/cy4cu5b/, ale ten shuffle nebyl vubec potreba
 - day 15 - zajimave na generovani vsech kombinaci
 - day 13 - Dijkstra algorithm with oriented edges
 - day 10 - Quite interesting, I had to tweak a string construction, ideal for usage with link_profiler
--- day 19.py
+++ day 19.py
@@ -1,14 +1,18 @@
 import os
-def add_transition(transitions, src, dst):
-    if not transitions.get(src):
-        transitions[src] = []
-    transitions[src].append(dst)
+import json
+def add_transition(book, src, dst):
+    if not book['trans'].get(src):
+        book['trans'][src] = []
+    book['trans'][src].append(dst)
+    if book['trans_r'].get(dst):
+        raise ValueError
+    book['trans_r'][dst] = src
+    
 def walk_it(book):
     i = 0
     counter = 0
     
     while i < len(book['source']):
-        print(i)
         mod = 1
         valid = False
         if not book['trans'].get(book['source'][i]):
@@ -24,11 +28,47 @@ def walk_it(book):
                     book['results'][candidate] = True
         i+=mod
     return counter
+def dump_cache(cache):
+    script_name = os.path.basename(__file__)
+    filename = script_name.split('.')[0]+'.cache.json'
+    with open(filename,'w') as f:
+        json.dump(cache,f, indent=2)
+def read_cache():
+    return ""
+    script_name = os.path.basename(__file__)
+    filename = script_name.split('.')[0]+'.cache.json'
+    with open(filename,'r') as f:
+        cache = json.load(f)
+    return cache
+def reduce_it(book,medicine):
+    i = 0
+    global global_counter
+    global_counter+=1
+    if medicine == 'e':
+        return 0
+    best = -1
+    counter = 0
+    while medicine != 'e':
+        
+        for klic,value in book['trans_r'].items():
+            if klic not in medicine:
+                continue
+            medicine = medicine.replace(klic,value,1)
+            counter+=1
+
+                    
+        
+    return counter
 book = {
     'trans': {},
+    'trans_r': {},
     'source': '',
-    'results': {}
+    'results': {},
+    'cache': read_cache(),
+    'last_dump': 0,
+    'last_print': 0
 }
+global_counter = 0
 if __name__ == '__main__':
 # Get the name of the Python script
     script_name = os.path.basename(__file__)
@@ -38,12 +78,15 @@ if __name__ == '__main__':
         
         for line in f.readlines():
             line = line.strip()
+            if line == '#':
+                break
             if line == '':
                 continue
             if line.find('=>')>0:
                 [src,dst] = line.split(' => ')
-                add_transition(book['trans'], src,dst)
+                add_transition(book, src,dst)
             else:
                 book['source'] = line
-    print(book,walk_it(book),len(book['results']))
+    print(walk_it(book),len(book['results']))
+    print("Zde",reduce_it(book,book['source']))
     
